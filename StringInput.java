14e0b9dc23bb3443bdc684be8a5ddfd66f91ec94
Shrutiiiie09 <shrutishevade32@gmail.com>
2026-08-06 23:38:00 +0530

    Added StringInput program

---
+++ StringInput.java
@@ -0,0 +1,17 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
+package GitHub;
+import java.util.Scanner;
+
+public class StringInput {
+   public StringInput() {
+   }
+
+   public static void main(String[] var0) {
+      Scanner sc = new Scanner(System.in);
+      String var2 = sc.next();
+      int var3 = sc.nextInt();
+      System.out.println(var2);
+      System.out.println(var3);
+      sc.close();
+   }
+}
