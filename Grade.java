f914cbdf99a0ec99b71cea3690215d9220ea7140
Shrutiiiie09 <shrutishevade32@gmail.com>
2026-08-07 23:31:37 +0530

    Grade showing Program

---
+++ Grade.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Grade {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int marks = sc.nextInt();
+        if(marks >=90){
+            System.out.println("A"); 
+        }
+        else if(marks >= 75 ){
+             System.out.println("B"); 
+        }
+        else if(marks >= 60 ){
+             System.out.println("C"); 
+        }
+        else if(marks >= 40 ){
+             System.out.println("D"); 
+        }
+        else{
+            System.out.println("F"); 
+        }
+        sc.close();
+        }
+            
+    }
+
