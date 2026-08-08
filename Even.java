cb5d66ced2f4dbeea31eb4289ca2e099fb2e3408
Shrutiiiie09 <shrutishevade32@gmail.com>
2026-08-09 00:01:29 +0530

    1-50 even numbers using for loop

---
+++ Even.java
@@ -0,0 +1,9 @@
+public class Even {
+    public static void main(String[] args) {
+       
+        for (int num = 1; num <= 50; num++) {
+            if (num % 2==0)
+            System.out.println(num);
+        }
+    }
+}
