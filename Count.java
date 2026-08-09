ff79f2a471ec61d8593a2d0ef76fc6f1e07c9b40
Shrutiiiie09 <shrutishevade32@gmail.com>
2026-08-10 00:18:36 +0530

    counting the numbers of input

---
+++ Count.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Count {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int i = sc.nextInt();
+        int count = 0;
+
+        while (i != 0) {
+            i = i / 10;
+            count++;
+        }
+
+        System.out.println(count);
+
+        sc.close();
+    }
+}
