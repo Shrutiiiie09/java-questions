79a74369629da5f8ac0b62ac8a9b4ff3f6735efd
Shrutiiiie09 <shrutishevade32@gmail.com>
2026-08-17 22:11:28 +0530

    updated

--- Circle.java
+++ Circle.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Circle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("enter radius");
 
         double radius = sc.nextDouble();
 
