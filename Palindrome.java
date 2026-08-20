8b2b6b790c0320ba3a66e2f51231277a84985abf
Shrutiiiie09 <shrutishevade32@gmail.com>
2026-08-20 22:58:51 +0530

    palindrome

---
+++ Palindrome.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number");
+        int n = sc.nextInt();
+
+        int original = n;
+        int rev = 0;
+
+        while (n != 0) {
+            int digit = n % 10;
+            rev = rev * 10 + digit;
+            n = n / 10;
+        }
+
+        if (original == rev) {
+            System.out.println("is palindrome");
+        } else {
+            System.out.println("is not palindrome");
+        }
+
+        sc.close();
+    }
+}
