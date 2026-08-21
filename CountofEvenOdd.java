f7adc5026a72d98fd6af4bd44c726e7c07955757
Shrutiiiie09 <shrutishevade32@gmail.com>
2026-08-21 19:09:18 +0530

    updated count of even & odd numbers from input

--- CountofEvenOdd.java
+++ CountofEvenOdd.java
@@ -1,32 +1,33 @@
 import java.util.Scanner;
+
 public class CountofEvenOdd {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
+
         int N = sc.nextInt();
         int even = 0;
         int odd = 0;
         int zero = 0;
 
-        while(N != 0){
+        while (N != 0) {
             int digit = N % 10;
             N = N / 10;
 
-            if(digit % 2 ==0){
-                System.out.println(even);
-            }
-            else if(N % 2 !=0){
-                System.out.println(odd);
-            }
-            else{
-                System.out.println(zero);
+            if (digit == 0) {
+                zero++;
+            } 
+            else if (digit % 2 == 0) {
+                even++;
+            } 
+            else {
+                odd++;
             }
         }
-        System.out.println("even numbers =" +even);
-        System.out.println("odd numbers =" +odd);
-        System.out.println("zero numbers =" +zero);
 
-        
+        System.out.println("even numbers = " + even);
+        System.out.println("odd numbers = " + odd);
+        System.out.println("zero numbers = " + zero);
 
+        sc.close();
     }
-    
 }
