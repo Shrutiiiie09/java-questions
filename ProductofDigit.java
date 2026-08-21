6dfd5b98ce13abac65e57ded0e130c48953d8e35
Shrutiiiie09 <shrutishevade32@gmail.com>
2026-08-21 22:28:57 +0530

    product of given numbers

---
+++ ProductofDigit.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class ProductofDigit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number");
+        int N = sc.nextInt();
+        int product = 1;
+        int digit;
+        while(N != 0) {
+            digit = N % 10;
+            product = product * digit;
+            N = N / 10;
+        }
+        System.out.println("Product of digit is = " +product);
+        sc.close();
+    }
+}
