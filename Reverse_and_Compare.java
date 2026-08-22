417d0f3cc15ccaacc7d73005419d37108e12e29e
Shrutiiiie09 <shrutishevade32@gmail.com>
2026-08-22 07:41:08 +0530

    Take a number, reverse it, and compare it with the original.

---
+++ Reverse_and_Compare.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Reverse_and_Compare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int original = n;
+        int rev = 0;
+
+        while (n != 0){
+            int digit = n % 10;
+            rev = rev * 10 + digit;
+            n = n / 10;
+        }
+
+        if(original == rev) {
+            System.out.println("is a palindrome");
+        }
+        else{
+            System.out.println("is not a palindrome");
+        }
+        sc.close();
+    }
+    
+}
