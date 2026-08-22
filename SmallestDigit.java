c3762f2ff3341b096dbffaa9942f5de1d6e9e647
Shrutiiiie09 <shrutishevade32@gmail.com>
2026-08-22 23:05:37 +0530

    find the smallest digit

--- SmallestDigit.java
+++ SmallestDigit.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class SmallestDigit {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-       
+        System.out.print("Enter number = ");
         int n = sc.nextInt();
         int smallest = n;
         int digit;
