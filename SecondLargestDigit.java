7d30c8120ef54a01260900fc87ca9350c97869e0
Shrutiiiie09 <shrutishevade32@gmail.com>
2026-08-22 23:27:09 +0530

    find the second largest digit.

---
+++ SecondLargestDigit.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class SecondLargestDigit {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int largest = -1;
+        int secondLargest = -1;
+        int digit;
+
+        while (n != 0) {
+
+            digit = n % 10;
+
+            if (digit > largest) {
+                secondLargest = largest;
+                largest = digit;
+            }
+            else if (digit > secondLargest && digit != largest) {
+                secondLargest = digit;
+            }
+
+            n = n / 10;
+        }
+
+        System.out.println("Largest digit = " + largest);
+        System.out.println("Second largest digit = " + secondLargest);
+
+        sc.close();
+    }
+}
