105bfae55bd57ea31d4b16802fa2db7a6e73baa3
Shrutiiiie09 <shrutishevade32@gmail.com>
2026-08-23 22:42:20 +0530

    printing prime number i range.

---
+++ PrimeNumbersInRange.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class PrimeNumbersInRange {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        
+        int start = sc.nextInt();
+
+        System.out.print("Enter end: ");
+        int end = sc.nextInt();
+
+        for (int i = start; i <= end; i++) {
+
+            boolean isPrime = true;
+
+            // Numbers less than 2 are not prime
+            if (i < 2) {
+                isPrime = false;
+            } else {
+
+                for (int j = 2; j < i; j++) {
+
+                    if (i % j == 0) {
+                        isPrime = false;
+                        break;
+                    }
+                }
+            }
+
+            if (isPrime) {
+                System.out.print(i + " ");
+            }
+        }
+
+        sc.close();
+    }
+}
