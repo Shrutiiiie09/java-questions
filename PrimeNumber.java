09c6bab774efe8aef26b867e74843da99a8fe911
Shrutiiiie09 <shrutishevade32@gmail.com>
2026-08-23 13:19:44 +0530

    find the number is prime or not

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -4,6 +4,8 @@ public class PrimeNumber {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter number = ");
+
         int n = sc.nextInt();
         boolean isPrime = true;
 
