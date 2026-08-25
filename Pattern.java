61df10fa5303ae37ea18a3f809c23fd33762b175
Shrutiiiie09 <shrutishevade32@gmail.com>
2026-08-25 06:49:58 +0530

    printing pattern in rectangular shape

--- Pattern.java
+++ Pattern.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
         public static void main(String args[]){
             Scanner sc = new Scanner(System.in);
 
+            System.out.print("Enter row = ");
             int n = sc.nextInt();
+            System.out.print("Enter columns = ");
             int m = sc.nextInt();
 
             for (int i = 1; i <= n; i++) {
