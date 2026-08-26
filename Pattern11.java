0f55d45bf30b57777148d07b3ee27991dc307ddc
Shrutiiiie09 <shrutishevade32@gmail.com>
2026-08-26 07:06:35 +0530

    printing solid rhombus pattern

--- Pattern11.java
+++ Pattern11.java
@@ -4,7 +4,7 @@ public class Pattern11 {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter number = ");
+        System.out.print("Enter number: ");
         int n = sc.nextInt();
 
         for(int i=1; i<=n; i++){
