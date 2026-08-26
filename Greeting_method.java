d1fb1ae181801a4e807c9c9d0d5519e5360df4d5
Shrutiiiie09 <shrutishevade32@gmail.com>
2026-08-26 21:28:33 +0530

    printing hello world using method

---
+++ Greeting_method.java
@@ -0,0 +1,10 @@
+public class Greeting_method {
+
+    static void greet() {
+        System.out.println("Hello, welcome to Java!");
+    }
+
+    public static void main(String[] args) {
+        greet();
+    }
+}
