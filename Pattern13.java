a7281590cc08ddfbe23724a50c46858f60815d3f
Shrutiiiie09 <shrutishevade32@gmail.com>
2026-08-26 19:42:57 +0530

    palindrome pyramid.

---
+++ Pattern13.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Pattern13 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number = ");
+        int n = sc.nextInt();
+
+        for(int i=1; i<=n; i++){
+            //spaces
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+
+            //for 1st half
+            for(int j=i; j>=1; j--){
+                System.out.print(j);
+            }
+
+            //for 2nd half
+            for(int j=2; j<=i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
