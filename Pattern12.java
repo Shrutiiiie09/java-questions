ce9db9363cf9ae3f23ce86074ef9ea088dce13c3
Shrutiiiie09 <shrutishevade32@gmail.com>
2026-08-26 19:15:45 +0530

    perfect pyramid of number.

--- Pattern12.java
+++ Pattern12.java
@@ -3,6 +3,7 @@ public class Pattern12 {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter number = ");
         int n = sc.nextInt();
 
         for(int i=1; i<=n; i++){
