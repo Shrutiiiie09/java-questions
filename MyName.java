6d5dbd2cc7263d3e51db59cc03216ed479bf02ca
Shrutiiiie09 <shrutishevade32@gmail.com>
2026-08-28 22:31:11 +0530

    updated myname program using method

--- MyName.java
+++ MyName.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class MyName {
 
     static void greet(String name) {
-        System.out.println("Hello, " + name);
+        System.out.println("Hello!!!, " + name);
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter your name: ");
+        System.out.print("Enter your name = ");
         String name = sc.nextLine();
 
         greet(name);
